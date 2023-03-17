6a5ae2b0a33eb6e2d1e747a9052367329d9b621c
Rifaatislaam <mdrifaatislaam@gmail.com>
2023-03-17 12:03:39 +0600

    Responsive section design

--- Javascript/script.js
+++ Javascript/script.js
@@ -6,4 +6,25 @@ const navMenu = document.getElementById("nav-menu"),
   // OPEN AND CLOSE MENU 
 navToggle.addEventListener("click", () => {
   navMenu.classList.toggle("nav-menu-open");
+  changeIcon();
 });
+
+
+//close the menu when the user clicks the nav link
+navItem.forEach((item) => {
+  item.addEventListener("click", () => {
+    if (navMenu.classList.contains("nav-menu-open")) {
+      navMenu.classList.remove("nav-menu-open");
+    }
+    changeIcon();
+  });
+});
+
+//change nav toggle icon 
+function changeIcon() {
+  if (navMenu.classList.contains("nav-menu-open")) {
+    navToggle.classList.replace("ri-menu-3-line", "ri-close-fill");
+  } else {
+    navToggle.classList.replace("ri-close-fill", "ri-menu-3-line" );
+  }
+}
